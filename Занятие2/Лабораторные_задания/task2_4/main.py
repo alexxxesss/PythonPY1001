8ad258242b7f5aec6d427e9c0a930bab0499271a
alexxxesss <alexeenkoalexxx@yandex.ru>
2021-10-21 20:39:37 +0300

    ok

--- Занятие2/Лабораторные_задания/task2_4/main.py
+++ Занятие2/Лабораторные_задания/task2_4/main.py
@@ -12,3 +12,14 @@ if __name__ == "__main__":
     # постарайтесь не использовать "магические" числа,
     # а по возможности дать переменным осмысленные названия и использовать их
     # TODO Распечатать строку лесенкой
+
+    count_whitespace = 5
+    whitespace = " "
+    str_ = 'Hello, world'
+
+    for i in range(len(str_)):
+        if i < 1:
+            print(whitespace * count_whitespace + str_[i])
+        else:
+            count_whitespace += 1
+            print(whitespace * count_whitespace + str_[i])

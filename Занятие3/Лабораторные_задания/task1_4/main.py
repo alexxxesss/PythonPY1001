b226fab835fad1b08e753a0b79ec4b8e08e77fd5
Alex-PC-home <alexeenkoalexxx@yandex.ru>
2021-10-26 02:17:36 +0300

    okey 26.10

--- Занятие3/Лабораторные_задания/task1_4/main.py
+++ Занятие3/Лабораторные_задания/task1_4/main.py
@@ -2,11 +2,10 @@ if __name__ == "__main__":
     def infinity(epsilon=0.0001):
         sum_ = 0
         n = 1
-        while True:
-            if 1 / (2**n) > epsilon:
+        while 1 / (2**n) > epsilon:
                 sum_ += 1 / (2**n)
                 n += 1
-            else:
-                break
-        return (sum_)
+
+        return sum_
+
     print(infinity())
